36e01fba712fc184f742455d90d6a3e0dc7c9c75
Miguel Ferro <c.miguel.ferro@gmail.com>
2022-12-05 13:58:54 -0500

    programa correo creado

---
+++ correo.py
@@ -0,0 +1,9 @@
+#programa que permite creación correo
+
+edad = int(input("Ingrese su edad: "))
+
+if edad >=8:
+    nombre = input("ingrese su primer nombre o nombre favorito: ")
+    print("Su correo es: "+nombre+"@gmail.com")
+else:
+    print("Tas muy chiqui para hablar por hotmail")
